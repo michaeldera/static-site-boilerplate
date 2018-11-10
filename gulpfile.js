7681a72890687297a1c36308ddd9f8bbafadc835
Michael Dera <michael@devshop.co.zw>
2018-11-10 16:41:35 +0200

    added gulp preprocessors for sass

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,16 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', () =>{
-    return gulp.src('src/scss/styles.scss')
+    return gulp.src('src/scss/**/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('dist/css'))
 });
 
 gulp.task('init', () => {
     console.log("starting build");
+});
+
+gulp.task('watch', () =>{
+    gulp.watch('src/scss/**/*.scss', ['sass']);
+    //other glup watch functions can go here 
 });
