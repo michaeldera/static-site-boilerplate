9690978703bd1bd3a801048c6364b509ed19f2d5
Michael Dera <michael@devshop.co.zw>
2018-11-10 16:57:51 +0200

    added gulp watch for files

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,33 @@
 var gulp = require('gulp');
 //Plugin for preprocessing sass
 var sass = require('gulp-sass');
+//Browser sync for live-reloading 
+var browserSync = require('browser-sync').create();
+
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+      server: {
+        baseDir: 'dist'
+      },
+    })
+  });
+
 
 gulp.task('sass', () =>{
     return gulp.src('src/scss/**/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.reload({
+            stream:true
+        }))
 });
 
-gulp.task('watch', () =>{
+
+gulp.task('watch', ['browserSync', 'sass'], () =>{
     gulp.watch('src/scss/**/*.scss', ['sass']);
+    gulp.watch('src/*.html', browserSync.reload); 
+    gulp.watch('src/js/**/*.js', browserSync.reload); 
     //other glup watch functions can go here 
 });
+
