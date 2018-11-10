78bb6b75f927d8342db039ffc8834853d874ba39
Michael Dera <michael@devshop.co.zw>
2018-11-10 16:31:23 +0200

    added sass stylesheet

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 var gulp = require('gulp');
+//Plugin for preprocessing sass
+var sass = require('gulp-sass');
+
+gulp.task('sass', () =>{
+    return gulp.src('src/scss/styles.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('dist/css'))
+});
 
 gulp.task('init', () => {
     console.log("starting build");
-})
+});
