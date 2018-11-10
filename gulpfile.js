162b1da134612d4e0ee29ce76de330980d00e7bc
Michael Dera <michael@devshop.co.zw>
2018-11-10 18:46:35 +0200

    added gulp optimisation tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,30 @@ var sass = require('gulp-sass');
 //Browser sync for live-reloading 
 var browserSync = require('browser-sync').create();
 
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+
+gulp.task('useref', function(){
+    return gulp.src('src/*.html')
+      .pipe(useref())
+      .pipe(gulpIf('*.js', uglify()))
+      // Minifies only if it's a CSS file
+      .pipe(gulpIf('*.css', cssnano()))
+      .pipe(gulp.dest('dist'))
+  });
+
+  gulp.task('images', function(){
+    return gulp.src('src/images/**/*.+(png|jpg|jpeg|gif|svg)')
+    // Caching images that ran through imagemin
+    .pipe(cache(imagemin({
+        interlaced: true
+      })))
+    .pipe(gulp.dest('dist/images'))
+  });
 
 gulp.task('browserSync', function() {
     browserSync.init({
@@ -13,7 +37,6 @@ gulp.task('browserSync', function() {
     })
   });
 
-
 gulp.task('sass', () =>{
     return gulp.src('src/scss/**/*.scss')
         .pipe(sass())
@@ -22,7 +45,6 @@ gulp.task('sass', () =>{
             stream:true
         }))
 });
-
 
 gulp.task('watch', ['browserSync', 'sass'], () =>{
     gulp.watch('src/scss/**/*.scss', ['sass']);
