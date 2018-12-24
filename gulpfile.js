3c9c719d7f6d35be18d0baec0bb1105c37c2f912
Michael Dera <michael@devshop.co.zw>
2018-12-24 10:36:44 +0200

    gulp 4 syntax

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { series, parallel } = require(gulp);
+const { src, dest, series, parallel, watch } = require(gulp);
 
 const gulp = require('gulp');
 //Plugin for preprocessing sass
@@ -13,69 +13,55 @@ const cssnano = require('gulp-cssnano');
 const imagemin = require('gulp-imagemin');
 const cache = require('gulp-cache');
 const del = require('del');
-const runSequence = require('run-sequence');
 
-gulp.task('useref', () => {
-    return gulp.src('src/*.html')
-      .pipe(useref())
-      .pipe(gulpIf('*.js', uglify()))
-      // Minifies only if it's a CSS file
-      .pipe(gulpIf('*.css', cssnano()))
-      .pipe(gulp.dest('dist'))
-  });
 
-  gulp.task('images', () => {
-    return gulp.src('src/images/**/*.+(png|jpg|jpeg|gif|svg)')
+function useref() {
+  return src('src/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    // Minifies only if it's a CSS file
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(dest('dist'));
+}
+
+function optimiseImages() {
+  return src('src/images/**/*.+(png|jpg|jpeg|gif|svg)')
     // Caching images that ran through imagemin
     .pipe(cache(imagemin({
-        interlaced: true
-      })))
-    .pipe(gulp.dest('dist/images'))
-  });
+      interlaced: true
+    })))
+    .pipe(dest('dist/images'))
+}
 
-  gulp.task('fonts', () => {
-    return gulp.src('src/fonts/**/*')
-    .pipe(gulp.dest('dist/fonts'))
-  });
+function moveFonts() {
+  return src('src/fonts/**/*')
+    .pipe(dest('dist/fonts'))
+}
 
-  gulp.task('clean:dist', () => {
-    return del.sync('dist');
-  });
+function clean(){
+      return del.sync('dist');
+}
 
-gulp.task('browserSync', () => {
-    browserSync.init({
-      server: {
-        baseDir: 'dist'
-      },
-    })
-  });
+function liveReload( callback){
+    browserSync.init({server: { baseDir: 'dist' }});
+    callback();
+}
 
-gulp.task('sass', () =>{
-    return gulp.src('src/scss/**/*.scss')
-        .pipe(sass())
-        .pipe(cssnano())
-        .pipe(gulp.dest('dist/css'))
-        .pipe(browserSync.reload({
-            stream:true
-        }))
-});
+function cssTranspile() {
+  return src('src/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(cssnano())
+    .pipe(gulp.dest('dist/css'));
+}
 
-gulp.task('watch', ['browserSync', 'sass'], () =>{
-    gulp.watch('src/scss/**/*.scss', ['sass']);
-    gulp.watch('src/*.html', browserSync.reload); 
-    gulp.watch('src/js/**/*.js', browserSync.reload); 
-    //other glup watch functions can go here 
-});
+function watchChanges(callback){
+  watch(['src/scss/**/*.scss', 'src/*.html', 'src/js/**/*.js' ], build);
+  callback();
+}
+function build(callback){
+  series( clean, parallel(cssTranspile, moveFonts, optimiseImages, useref));
+  callback();
+}
 
-gulp.task('build',  (callback) => {
-    runSequence('clean:dist', 
-      ['sass', 'useref', 'images', 'fonts'],
-      callback
-    )
-  })
-
-  gulp.task('default', (callback) => {
-    runSequence(['sass','browserSync', 'watch'],
-      callback
-    )
-  })
+exports.build = build;
+exports.default = series( build, liveReload, watchChanges);
